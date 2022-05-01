07cfaf272e664aeace2e04649751f93920cde841
wak10 <chiba.r25.taka@gmail.com>
2022-05-01 11:35:53 +0900

    Initial Commit

---
+++ Shared/Map_originalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_originalApp.swift
+//  Shared
+//
+//  Created by Takahiro Chiba on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct Map_originalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
