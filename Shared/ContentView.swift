f7dc7c21c1e649625b2dc25c3c1d768bfabd6f4b
wak10 <chiba.r25.taka@gmail.com>
2022-05-06 22:40:49 +0900

    [add]

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -4,47 +4,22 @@
 //
 //  Created by Takahiro Chiba on 2022/05/01.
 //
-
-import SwiftUI
+import UIKit
 import MapKit
 
-struct ContentView: View {
-    typealias Body = <#type#>
-    
-    class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController {
+   @IBOutlet weak var mapView: MKMapView!
 
-        // storyboardにMapViewを置き、それと接続する
-        @IBOutlet var mapView:MKMapView!
+   override func viewDidLoad() {
+       super.viewDidLoad()
+       
+   }
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
 
-            // 緯度・軽度を設定
-            let location:CLLocationCoordinate2D
-                = CLLocationCoordinate2DMake(35.68154,139.752498)
-
-            mapView.setCenter(location,animated:true)
-
-            // 縮尺を設定
-            var region:MKCoordinateRegion = mapView.region
-            region.center = location
-            region.span.latitudeDelta = 0.02
-            region.span.longitudeDelta = 0.02
-
-            mapView.setRegion(region,animated:true)
-
-            // 表示タイプを航空写真と地図のハイブリッドに設定
-            mapView.mapType = MKMapType.hybrid
-            // mapView.mapType = MKMapType.standard
-            // mapView.mapType = MKMapType.satellite
-        }
-
-    }
 }
 
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct MapView_Previews: PreviewProvider {
+    static var previews: some <#type#> = View {
+        MapView()
     }
 }
