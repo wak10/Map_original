9ba9242110a42fa799a6cc1da6a7c8f30a9931ed
wak10 <chiba.r25.taka@gmail.com>
2022-05-14 13:35:43 +0900

    [add]

--- Shared/Map_originalApp.swift
+++ Shared/Map_originalApp.swift
@@ -7,8 +7,14 @@
 
 import SwiftUI
 
+//APIキーの宣言
+let API_KEY = "API_KEY"
+
 @main
 struct Map_originalApp: App {
+    //AppDelegateと接続するアダプタを宣言
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
